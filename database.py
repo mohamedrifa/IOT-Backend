d8a014217984272ed5119e338674031e77834ec4
Mohamed Rifayath <141167029+mohamedrifa@users.noreply.github.com>
2026-02-17 10:30:18 +0530

    17/02/2025

--- database.py
+++ database.py
@@ -9,10 +9,12 @@ DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASS = os.getenv("DB_PASS", "root")
 DB_NAME = os.getenv("DB_NAME", "iot_db")
+DB_PORT = os.getenv("DB_PORT", 3306)
 
-DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DB_URL, echo=True)
+
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
